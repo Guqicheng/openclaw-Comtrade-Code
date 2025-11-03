5f0bd2d8ffab85c7f976d815315a380dac86b524
顾七城 <2821835846@qq.com>
2025-11-03 16:18:12 +0800

    1.2.6.2 打包所有需要修改

--- backend/app.py
+++ backend/app.py
@@ -5,26 +5,40 @@ from flask import Flask, send_from_directory
 from backend.routes import bp
 from backend.config import UPLOAD_FOLDER
 import os
+import webbrowser
+import threading
+
 
 def create_app():
     app = Flask(__name__)
     app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-
-    app.config['DEBUG'] = True  # 生产环境需关闭！
+    app.config['DEBUG'] = True  # 注意：正式环境要关闭
 
     os.makedirs(UPLOAD_FOLDER, exist_ok=True)
     app.register_blueprint(bp)
 
     @app.route('/')
     def index():
-        return send_from_directory('../frontend', 'index.html')
+        return send_from_directory('../web', 'index.html')
 
     @app.route('/<path:path>')
     def static_proxy(path):
-        return send_from_directory('../frontend', path)
+        return send_from_directory('../web', path)
 
     return app
 
+
+def open_browser(port=7000):
+    """延迟 1 秒打开浏览器，避免 Flask 尚未启动完毕"""
+    url = f"http://127.0.0.1:{port}"
+    webbrowser.open(url)
+
+
 if __name__ == '__main__':
     app = create_app()
-    app.run(debug=True, host='0.0.0.0',port=7000)
+
+    port = 7000
+    threading.Timer(1.0, open_browser, args=(port,)).start()
+
+    app.run(debug=True, host='0.0.0.0', port=port)
+
